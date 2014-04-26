1ebcd185f117d7f88d0e036fd8338e21edf7b1eb
JonathanOta <jonathanota1@gmail.com>
2014-04-25 22:27:47 -0700

    video timing biiiaattcchh
    getting video.duration stuff and using setTimeOut’s to enable proper
    var loading.

--- script3.js
+++ script3.js
@@ -28,7 +28,7 @@ var prevVideoClass = $('li.currentVideo');
 var prevImageClass = $('li.currentImage');
 var prevTrackNumberClass = $('li.currentTrackNumber');
 
-
+var videoDuration = 0;
 
 
 
@@ -39,6 +39,10 @@ var prevTrackNumberClass = $('li.currentTrackNumber');
 var a = audiojs.createAll({
 trackEnded: function() {
 	
+ 	nextVideoClass = $('li.currentVideo').next();
+		if(!nextVideoClass.length) nextVideoClass = $('ol.videoTracks li').first();
+			nextVideoClass.incrementVideoClass();
+	 
 	 nextAudioClass = $('li.playing').next(); // creates a variable that stores content of sibling
 		if (!nextAudioClass.length) nextAudioClass = $('ol.audioArray li').first();
 			nextAudioClass.incrementAudioClass();	
@@ -47,10 +51,6 @@ trackEnded: function() {
 		if(!nextTrackClass.length) nextTrackClass = $('ol.trackNames li').first();
 			nextTrackClass.incrementTrackClass();
 		
-		
-	nextVideoClass = $('li.currentVideo').next();
-		if(!nextVideoClass.length) nextVideoClass = $('ol.videoTracks li').first();
-			nextVideoClass.incrementVideoClass();
 	
 	nextImageClass = $('li.currentImage').next();
 		if(!nextImageClass.length) nextImageClass = $('ol.images li').first();
@@ -65,70 +65,6 @@ trackEnded: function() {
 	
 		
 });
-
-
-
-// Load in the first track
-var audio = a[0];
-var first = $('ol.audioArray a').attr('data-src');
-$('ol.audioArray li').first().addClass('playing');
-/* audio.load(first); */
-
-
-$.fn.incrementAudioClass = function(){
-	
-	$(this)
-		.addClass('playing')
-		.siblings()
-		.removeClass('playing'); 
-
-//returns song url to be loaded and played			
-	audio.load($('a', this).attr('data-src')); 
-	
-	//wait to play audio during video
-	setTimeout(function(){audio.play()}, 8000); 
-	
-}
-
-
-$.fn.incrementTrackClass = function(){
-	
-	 $(this)
-	 	.removeClass('inactiveTrack')
-		.addClass('currentTrack')
-		.siblings()
-		.removeClass('currentTrack')
-		.addClass('inactiveTrack');
-		
-
-
-//track title animation setup --> reset animation
-	$('li.inactiveTrack').animate({top: '220', opacity: '0'}, 2000).hide(); 
-	var trackTimeOut = setTimeout(function(){
-		$('li.currentTrack').show().animate({top: '200px', opacity: '1'}, 1000);
-	}, 17000);	
-						
-	
-}
-
-$.fn.incrementTrackNumberClass = function(){
-	
-		$(this)
-		.removeClass('inactiveTrackNumber')
-		.addClass('currentTrackNumber')
-		.siblings()
-		.removeClass('currentTrackNumber')
-		.addClass('inactiveTrackNumber');
-		
-//trackNumber animation setup reset --> animation
-	$('li.inactiveTrackNumber').animate({top: '70px', opacity: '1'}, 2000).hide();	
-	setTimeout(function(){
-		$('li.currentTrackNumber').show().animate({top: '100px', opacity: '1'}, 1000)
-	}, 17000);
-	
-}
-
-
 
 $.fn.incrementVideoClass = function(){
 	
@@ -152,6 +88,12 @@ $.fn.incrementVideoClass = function(){
 	video.load();
 	video.play();	
 	video.volume=.75;
+
+//find how long video is to calculate timing from
+	video.addEventListener ('loadedmetadata', function(){
+		videoDuration = (video.duration)*1000;
+ 		console.log("video loop" + ' ' + videoDuration); 
+	});
 	
 //fade video when video ends	
 	$('li.currentVideo video').on('ended', function(){
@@ -159,6 +101,81 @@ $.fn.incrementVideoClass = function(){
 	});
 
 }
+
+
+
+// Load in the first track
+var audio = a[0];
+var first = $('ol.audioArray a').attr('data-src');
+$('ol.audioArray li').first().addClass('playing');
+/* audio.load(first); */
+
+
+$.fn.incrementAudioClass = function(){
+	
+	$(this)
+		.addClass('playing')
+		.siblings()
+		.removeClass('playing'); 
+
+//returns song url to be loaded and played			
+	audio.load($('a', this).attr('data-src')); 
+	
+	//wait to play audio during video
+setTimeout(function(){	
+		setTimeout(function(){audio.play()}, (videoDuration - videoDuration/3)); 
+	}, 250);
+	
+	console.log("audio timing" + ' ' + videoDuration - videoDuration/3);
+}
+
+
+$.fn.incrementTrackClass = function(){
+	
+	 $(this)
+	 	.removeClass('inactiveTrack')
+		.addClass('currentTrack')
+		.siblings()
+		.removeClass('currentTrack')
+		.addClass('inactiveTrack');
+		
+
+
+//track title animation setup --> reset animation
+	$('li.inactiveTrack').animate({top: '220', opacity: '0'}, 2000).hide(); 
+
+setTimeout(function (){
+	setTimeout(function(){
+		$('li.currentTrack').show().animate({top: '200px', opacity: '1'}, 1000);
+	}, videoDuration+750);	
+	
+	console.log("track loop" + ' ' + videoDuration+500);
+						
+	}, 200);
+}
+
+$.fn.incrementTrackNumberClass = function(){
+	
+		$(this)
+		.removeClass('inactiveTrackNumber')
+		.addClass('currentTrackNumber')
+		.siblings()
+		.removeClass('currentTrackNumber')
+		.addClass('inactiveTrackNumber');
+		
+//trackNumber animation setup reset --> animation
+	$('li.inactiveTrackNumber').animate({top: '70px', opacity: '1'}, 2000).hide();	
+
+setTimeout(function (){
+	setTimeout(function(){
+		$('li.currentTrackNumber').show().animate({top: '100px', opacity: '1'}, 1000)
+	}, videoDuration+750);
+	
+	console.log("track Number loop" + ' ' + videoDuration+500);
+	
+	}, 200);
+}
+
 
 
 $.fn.incrementImageClass = function(){
@@ -196,6 +213,11 @@ if (code == 39) {
 		var source = document.querySelectorAll('li.currentVideo video source');
 		video.pause();
 
+//------Video incrementor
+		nextVideoClass = $('li.currentVideo').next();
+		if(!nextVideoClass.length) nextVideoClass = $('ol.videoTracks li').first();
+		nextVideoClass.incrementVideoClass();
+
 //audio incrementor
  nextAudioClass = $('li.playing').next();
 if (!nextAudioClass.length) nextAudioClass = $('ol.audioArray li').first();
@@ -205,11 +227,6 @@ nextAudioClass.incrementAudioClass();
 	nextTrackClass = $('li.currentTrack').next();
 	if(!nextTrackClass.length) nextTrackClass = $('ol.trackNames li').first();
 	nextTrackClass.incrementTrackClass();
-
-//------Video incrementor
-		nextVideoClass = $('li.currentVideo').next();
-		if(!nextVideoClass.length) nextVideoClass = $('ol.videoTracks li').first();
-		nextVideoClass.incrementVideoClass();
 		
 //----------Image incrementor		
 			nextImageClass = $('li.currentImage').next();
@@ -233,6 +250,12 @@ nextAudioClass.incrementAudioClass();
 		var source = document.querySelectorAll('li.currentVideo video source');
 		video.pause();
 
+//------video incrementor	
+		prevVideoClass = $('li.currentVideo').prev();
+		if(!prevVideoClass.length) prevVideoClass = $('ol.videoTracks li').last();
+		prevVideoClass.incrementVideoClass();
+		
+
 //audio incrementor
  prevAudioClass = $('li.playing').prev();
 if (!prevAudioClass.length) prevAudioClass = $('ol.audioArray li').last();
@@ -243,11 +266,7 @@ prevAudioClass.incrementAudioClass();
 	if(!prevTrackClass.length) prevTrackClass = $('ol.trackNames li').last();
 	prevTrackClass.incrementTrackClass();
 
-//------video incrementor	
-		prevVideoClass = $('li.currentVideo').prev();
-		if(!prevVideoClass.length) prevVideoClass = $('ol.videoTracks li').last();
-		prevVideoClass.incrementVideoClass();
-		
+
 //----------Image incrementor		
 			prevImageClass = $('li.currentImage').prev();
 			if(!prevImageClass.length) prevImageClass = $('ol.images li').last();
